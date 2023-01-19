312628befe98936f3fe908a94fc79753d59735b1
mrumbra361 <122780690+mrumbra361@users.noreply.github.com>
2023-01-19 12:56:48 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+let manito = 0
+input.onGesture(Gesture.Shake, function () {
+    manito += randint(1, 3)
+    if (manito == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+    	
+    }
+    if (true) {
+    	
+    }
+})
 basic.forever(function () {
 	
 })
